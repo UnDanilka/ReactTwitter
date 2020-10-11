0a6285bb2c0793174611496d965de41af8b1de43
Danil Elfimov <mr.elfimov@inbox.ru>
2020-10-11 16:14:41 +0300

    delete, add

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,20 +7,75 @@ import PostList from "../post-list/post-list";
 import PostAddForm from "../post-add-form/post-add-form"
 
 import "./app.css"
+import styled from "styled-components";
+import uuid from 'react-uuid'
+
+const AppBlock = styled.div `
+margin: 0 auto;
+max-width: 800px;
+`
+
+export default class App extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state={
+            data : [
+                {label: "Going to learn React", important: true , id: uuid()},
+                {label: "Thats awesome", important: false, id: uuid()},
+                {label: "I wanna sleep...", important: false, id: uuid()}
+            ]
+        }
+    }
 
 
-const App = () =>{
+    deleteItem = (id)=>{
+        this.setState(({data})=>{
+            const index = data.findIndex(elem=> elem.id === id);
+            const before = data.slice(0,index);
+            const after = data.slice(index + 1);
+            const newArr = [...before,...after];
+
+            return{
+                data: newArr
+            }
+        })
+
+    }
+
+    addItem = (body)=>{
+        const newItem = {
+            label: body,
+            important: false,
+            id: uuid()
+        }
+        console.log(newItem.id)
+        this.setState(({data})=>{
+            const newArr = [...data, newItem];
+            return{
+                data: newArr
+            }
+        })
+    }
+
+    render(){
+    
     return (
-        <div className="app">
+        <AppBlock>
             <AppHeader/>
         <div className="search-panel d-flex">
             <SearchPanel/>
             <PostStatusFilter/>
         </div>
-        <PostList/>
-        <PostAddForm/>
-        </div>
+        <PostList 
+        posts={this.state.data}
+        onDelete={this.deleteItem}/>
+        <PostAddForm
+        onAdd={this.addItem}/>
+        </AppBlock>
     )
 }
+    
 
-export default App;
+}
+
